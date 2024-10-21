773e68074c38e99c99d067b760548d9195a61af9
ankitsensei <main.ankitbhagat@gmail.com>
2024-10-21 11:33:23 +0530

    random password generator

---
+++ random_password/main.py
@@ -0,0 +1,20 @@
+# Generate Random Password
+
+import random
+import string
+
+
+def generatePassword(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+    
+length = 5
+
+print(f"The generated password is {generatePassword(length)}")
+
+
+
+
+
